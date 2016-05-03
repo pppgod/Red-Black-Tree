9be8d7da83fae4f9da46218ef03a944c87075240
eeSangyun L <Sangyun@LeeSangyunui-MacBook-Pro.local>
2016-05-03 21:19:28 +0900

    Initial Commit

---
+++ RedBlackExample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RedBlackExample
+//
+//  Created by LeeSangyun on 2016. 5. 3..
+//  Copyright © 2016년 LeeSangyun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
